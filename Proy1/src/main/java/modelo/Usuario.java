cac21eb02bfeb8cf4534dbf2c574628e2cf0fad5
Luis Castillo <luacastillom@udistrital.edu.co>
2024-02-21 10:50:40 -0500

    Origen

---
+++ Proy1/src/main/java/modelo/Usuario.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Usuario implements Serializable {
+    private String nombre;
+    public String correo;
+
+    public Usuario(String nombre, String correo) {
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombre=" + nombre + ", correo=" + correo + '}';
+    }
+    
+}
